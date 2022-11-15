49e468dd5984bdc4e96e438c6784622b7ef8f15e
Marrek Nožka <nozka@spseol.cz>
2022-11-15 12:59:26 +0100

    zaklad

--- main.py
+++ main.py
@@ -2,40 +2,40 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-
-# from tkinter import ttk
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
+from tkinter import HORIZONTAL, Scale
 
 
 class Application(tk.Tk):
-    name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "ColorMishMash"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
-        self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
+        #self.geometry('800x600')
 
-    def about(self):
-        window = About(self)
-        window.grab_set()
+        self.bind("<Escape>", self.quit)
+
+        self.lblHlavni = tk.Label(self, text="Hello World")
+        self.lblHlavni.pack(anchor='w')
+        self.btnQuit = tk.Button(self, text="Quit", command=self.quit)
+        self.btnQuit.pack(anchor='e')
+
+        self.frameR = tk.Frame(self)
+        self.frameR.pack()
+        self.frameG = tk.Frame(self)
+        self.frameG.pack()
+        self.frameB = tk.Frame(self)
+        self.frameB.pack()
+
+        self.lblR = tk.Label(self.frameR, text='R')
+        self.lblR.pack(side='left', anchor='s')
+        self.scaleR = Scale(self.frameR, from_=0, to=0xff, orient='horizontal', length=333)
+        self.scaleR.pack(side='left', anchor='s')
+        self.entryR = tk.Entry(self.frameR, width=4)
+        self.entryR.pack(side='left', anchor='s')
+
+        self.scaleG = Scale(self, from_=0, to=0xff, orient='horizontal', length=333)
+        self.scaleG.pack()
 
     def quit(self, event=None):
         super().quit()
